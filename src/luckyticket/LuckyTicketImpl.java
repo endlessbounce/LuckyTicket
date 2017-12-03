acc29ed84a4b5bb519af99ba24466c67d955a159
endlessbounce <Ican.Iwill-2017github>
2017-12-03 22:19:14 +0300

    optimized some algorithms

--- src/luckyticket/LuckyTicketImpl.java
+++ src/luckyticket/LuckyTicketImpl.java
@@ -1,34 +1,29 @@
 package luckyticket;
 
+import java.util.ArrayList;
+
 public class LuckyTicketImpl {
-    
-    public class LuckyNumber implements LuckyTicketInterface{
-        private long nums[];
+
+    public class LuckyNumber implements LuckyTicketInterface {
+        private long number;
         private long luckyTicketsNum;
+        private ArrayList<Long> list = new ArrayList<>();
 
-        private boolean numberCheck(String number){
+        private boolean numberCheck(String number) {
 
-            if(number.length() == 0){
+            if (number.length() == 0) {
                 System.out.println("The string must be of length 2+");
                 return false;
-            }else if(number.length() % 2 != 0){
+            } else if (number.length() % 2 != 0) {
                 System.out.println("The string must contain even number of digits");
                 return false;
             }
 
-            nums = new long[number.length()];
+            try {
 
-            try{    
+                this.number = Long.parseLong(number);
 
-                //convert the string to arr of integers
-                for(int i = 0; i < nums.length; i++){
-
-                    char temp = number.charAt(i);
-                    nums[i] = (long)Integer.parseInt(Character.toString(temp));
-
-                }
-
-            }catch(NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 System.out.println("Incorrect input string.");
                 return false;
             }
@@ -37,52 +32,83 @@ public class LuckyTicketImpl {
 
         }
 
-        @Override
-        public boolean isLucky(String number) {
-            boolean correctNum = numberCheck(number);
-            int firstNumsSum = 0;
-            int lastNumsSum = 0;
+        private long countSumm(long number, long sum) {
 
-            if(!correctNum){
-                return false;
+            if (number == 0) {
+                return sum;
             }
 
-            int i = 0;
+            sum += number % 10;
+            number /= 10;
 
-            while(i < number.length()/2){
-                firstNumsSum += nums[i];
-                i++;
-            }
+            sum = countSumm(number, sum);
 
-            while(i >= number.length()/2 && i < number.length()){
-                lastNumsSum += nums[i];
-                i++;
-            }
+            return sum;
+        }
 
-            if(firstNumsSum == lastNumsSum){
-                return true;
-            }else{
-                return false;
+        private void comparator(long number) {
+            long sum = countSumm(number, 0);
+
+            for (Long aList : list) {
+
+                if (aList == sum) luckyTicketsNum++;
+
             }
 
         }
 
         @Override
-        public long countLucky(long min, long max) {
-            Long num = min;
+        public boolean isLucky(String number) {
 
-            if(num.toString().length() != 12){
-                return 0L;
+            boolean correctNum = numberCheck(number);
+
+            if (!correctNum) {
+                return false;
             }
 
+            double power = number.length() / 2;
+
+            long leftHalf = (long) (this.number / Math.pow(10D, power));
+            long rightHalf = (long) (this.number % Math.pow(10D, power));
+
+            return countSumm(leftHalf, 0) == countSumm(rightHalf, 0);
+
+        }
+
+        @Override
+        public long countLucky(long min, long max) {
             luckyTicketsNum = 0;
 
-            for(long i = min; i <= max; i++){
-                boolean isLucky = isLucky((num++).toString());
+            if (Long.toString(min).length() != 12) return 0L;
 
-                if(isLucky){
-                    luckyTicketsNum++;
-                }
+            if (min > max) return 0L;
+
+            boolean minCheck = numberCheck(Long.toString(min));
+
+            if (!minCheck) return 0L;
+
+            long leftHalfMin = min / 1000000;
+            long leftHalfMax = max / 1000000;
+            long newSum;
+
+            //count sums of left-side numbers
+            while (leftHalfMin <= leftHalfMax) {
+
+                newSum = countSumm(leftHalfMin, 0);
+
+                list.add(newSum);
+
+                leftHalfMin++;
+            }
+
+            long rightHalfMin = min % 1000000;
+            long rightHalfMax = max % 1000000;
+
+            //compare sums of the right-side numbers to the sums of the left-side numbers
+            //while traversing the arrayList
+            while (rightHalfMin <= rightHalfMax) {
+                comparator(rightHalfMin);
+                rightHalfMin++;
             }
 
             return luckyTicketsNum;
@@ -90,56 +116,51 @@ public class LuckyTicketImpl {
 
         @Override
         public long countLucky(String min, String max) {
-            
-            if(min.length() != 2 || max.length() != 30){
+
+            if (min.length() != 2 || max.length() != 30) {
                 System.out.println("Incorrect input range (should be from 2 to 30 digits)");
                 return 0;
             }
-            
+
             luckyTicketsNum = 0;
             Long start = Long.valueOf(min);
             Long end = Long.valueOf(max);
-            boolean isLucky = false;
-            
-            while(start <= end){
+            boolean isLucky;
+
+            while (start <= end) {
                 String str = start.toString();
-                
-                if(str.length() % 2 != 0){
+
+                if (str.length() % 2 != 0) {
                     start++;
                     continue;
                 }
-                
+
                 isLucky = isLucky(str);
-                
-                if(isLucky){
+
+                if (isLucky) {
                     luckyTicketsNum++;
-                    
-                    if(luckyTicketsNum > Long.MAX_VALUE){
-                        System.out.println("Incorrect input range (should include lucky numbers no more than Long.MAX_VALUE)");
-                        return 0L;
-                    }
                 }
-                
+
                 start++;
             }
-            
+
             return luckyTicketsNum;
         }
 
     }
-    
+
     public static void main(String[] args) {
         LuckyTicketImpl.LuckyNumber numTest = new LuckyTicketImpl().new LuckyNumber();
-        
-        String ticket = "123601";
+
+        String ticket = "2241";
         boolean isLucky = numTest.isLucky(ticket);
         System.out.println(ticket + " is lucky: " + isLucky);
-        
-        long numOfLNums = numTest.countLucky(123456123456L, 123456123465L);
+
+        long numOfLNums = numTest.countLucky(123456654321L, 729496654321L);
         System.out.println("Found " + numOfLNums + " lucky numbers in the given range.");
-        
+
         long numOfLNums2 = numTest.countLucky("10", "000000000000000000000000128765");
         System.out.println("Found " + numOfLNums2 + " lucky numbers in the given range.");
-    }   
-    
+    }
+
 }
